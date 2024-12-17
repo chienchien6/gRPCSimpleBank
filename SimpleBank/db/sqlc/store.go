65dd574b3106258c115eac2d4b97a4e6d3851085
Chien-Chien <128012103+chienchien6@users.noreply.github.com>
2024-12-17 14:15:02 +0800

    update accounts' balance,ctx := context.WithValue(context.Background(), txKey, txName)找到deadlock原因,改成FOR NO KEY UPDATE;

--- SimpleBank/db/sqlc/store.go
+++ SimpleBank/db/sqlc/store.go
@@ -60,6 +60,9 @@ type TransferTxResult struct {
 	ToEntry       Entry    `json:"to_entry"`
 }
 
+var txKey = struct {
+}{}
+
 // TransferTx performs a money transfer from one account to the other.
 // It creates a transfer record,add account entries,and update accounts' balance within a single database transaction
 func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
@@ -68,6 +71,9 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
+		txName := ctx.Value(txKey)
+
+		fmt.Println(txName, "create transfer")
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: sql.NullInt64{Int64: arg.FromAccountID, Valid: true},
 			ToAccountID:   sql.NullInt64{Int64: arg.ToAccountID, Valid: true},
@@ -77,10 +83,7 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			return err
 		}
 
-		if err != nil {
-			return err
-		}
-
+		fmt.Println(txName, "create entry 1")
 		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: sql.NullInt64{Int64: arg.FromAccountID, Valid: true},
 			Amount:    -arg.Amount,
@@ -89,6 +92,7 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			return err
 		}
 
+		fmt.Println(txName, "create entry 2")
 		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
 			AccountID: sql.NullInt64{Int64: arg.ToAccountID, Valid: true},
 			Amount:    arg.Amount,
@@ -97,7 +101,33 @@ func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (Tr
 			return err
 		}
 
-		//TODO:update accounts' balance
+		fmt.Println(txName, "get account 1")
+		//get account -> update accounts' balance
+		account1, err := q.GetAccount(ctx, arg.FromAccountID)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "update account 1")
+		result.FromAccountID, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      account1.ID,
+			Balance: account1.Balance - arg.Amount,
+		})
+
+		fmt.Println(txName, "get account 2")
+		account2, err := q.GetAccount(ctx, arg.ToAccountID)
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(txName, "update account 2")
+		result.ToAccountID, err = q.UpdateAccount(ctx, UpdateAccountParams{
+			ID:      arg.ToAccountID,
+			Balance: account2.Balance + arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
 
 		return nil
 	})
--- SimpleBank/db/sqlc/store_test.go
+++ SimpleBank/db/sqlc/store_test.go
@@ -21,8 +21,11 @@ func TestTransferTx(t *testing.T) {
 	results := make(chan TransferTxResult)
 
 	for i := 0; i < n; i++ {
+		txName := fmt.Sprintf("tx %d", i+1)
+
 		go func() {
-			result, err := store.TransferTx(context.Background(), TransferTxParams{
+			ctx := context.WithValue(context.Background(), txKey, txName)
+			result, err := store.TransferTx(ctx, TransferTxParams{
 				FromAccountID: account1.ID,
 				ToAccountID:   account2.ID,
 				Amount:        amount,
@@ -36,6 +39,7 @@ func TestTransferTx(t *testing.T) {
 	existed := make(map[int]bool)
 
 	for i := 0; i < n; i++ {
+
 		err := <-errs
 		require.NoError(t, err)
 
